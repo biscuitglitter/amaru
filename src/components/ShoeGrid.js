2e16b75184407252c8571cf0b942f238f6277f5f
bobpetitchat <zermoun.yasmine@gmail.com>
2022-04-26 22:38:01 +0100

    set up firebase getdoc API call

--- src/components/ShoeGrid.js
+++ src/components/ShoeGrid.js
@@ -2,24 +2,31 @@ import React, { useState, useEffect } from "react"
 import { db } from "../firebase-config"
 import Shoe from "./Shoe"
 import { v4 as uuidv4 } from "uuid"
-import { collection, getDocs } from "firebase/firestore"
+import { onSnapshot, doc } from "firebase/firestore"
 
 const ShoeGrid = () => {
-
-  const shoesCollec = collection(db, "shoesdetails")
-  const [shoesArray, setShoesArray] = useState([])
+  const [shoes, setShoes] = useState([])
+  const docRef = doc(db, "shoesdetails", "JYEKETVDiu4hV0WbmJ1m")
 
   useEffect(() => {
-    const getArrays = async () => {
-      const data = await getDocs(shoesCollec)
-      setShoesArray(data.docs.map((doc) => ({ ...doc.data() })))
-      console.log(shoesArray)
+    onSnapshot(docRef, (doc) => {
+      setShoes(doc.data())
+    })
+  })
+
+  useEffect(() => {
+    console.log(shoes)
+    for (const obj in shoes) {
+      console.log(Object.keys(obj))
     }
-    getArrays()
+    
   }, [])
 
   return (
     <div className="grid grid-cols-4 grid-rows-3 gap-x-4 gap-y-8">
+      {/* {imageUrls.slice(0, 12).map((url) => {
+          return <Shoe key={uuidv4()} url={url} />
+        } */}
     </div>
   )
 }
